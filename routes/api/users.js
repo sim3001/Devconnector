1b35a28611d73090be1ceb6b5d2f075b315b2a79
Simranjit Singh <simranjit.boyal@gmail.com>
2018-07-22 08:57:57 -0600

    Added Register Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 //Load User Model
 const User = require("../../models/User");
 
@@ -23,6 +26,11 @@ router.get("/test", (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({
