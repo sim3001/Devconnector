75bcc57b64fd8b053a66b41239e0ea3752bb4c4d
Simranjit Singh <simranjit.boyal@gmail.com>
2018-07-23 20:18:23 -0600

    Created GET route to get a users Profile using the USER_ID for internal user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -62,6 +62,26 @@ router.get("/handle/:handle", (req, res) => {
     .catch(err => res.status(404).json(err));
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by id
+// @access  Public
+router.get("/user/:user_id", (req, res) => {
+  //Initalize errors object
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        //send 404 Status and send error
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route   POST api/profile
 // @desc    Create or Edit User Profile
 // @access  Private
