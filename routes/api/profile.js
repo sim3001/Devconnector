315f58555c7f5c6d1301d05901a6da81f24ac893
Simranjit Singh <simranjit.boyal@gmail.com>
2018-07-23 20:16:11 -0600

    Created GET route for

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -42,6 +42,26 @@ router.get(
   }
 );
 
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get("/handle/:handle", (req, res) => {
+  //Initalize errors object
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        //send 404 Status and send error
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route   POST api/profile
 // @desc    Create or Edit User Profile
 // @access  Private
