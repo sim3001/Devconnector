a9a2a81eb96efffba9d690e78aa2932a3a67c231
Simranjit Singh <simranjit.boyal@gmail.com>
2018-07-25 20:53:00 -0600

    Added DELETE Route for  Experience from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -237,4 +237,27 @@ router.post(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete Experience from profile
+// @access  Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Find Profile of logged in user
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // get remove index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+        //Splice out of array
+        profile.experience.splice(removeIndex, 1);
+        //Save profile
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
