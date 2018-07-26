30502b1eb4c30ab33acddddc224c6c9502113c25
Simranjit Singh <simranjit.boyal@gmail.com>
2018-07-25 21:34:18 -0600

    Changed from Local Database to mLab

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/devconnector", //"mongodb://sim:simsim1@ds223161.mlab.com:23161/devconnectorappdb"
+  mongoURI: "mongodb://sim:simsim1@ds223161.mlab.com:23161/devconnectorappdb",
   secretOrKey: "secretthatssupersecretive"
 };
