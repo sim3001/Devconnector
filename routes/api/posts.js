c4271e71cc5ed93409507e2dce21192be9d9bf61
Simranjit Singh <simranjit.boyal@gmail.com>
2018-09-12 22:29:22 -0600

    Added Create Post POST route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Import Post Model
+const Post = require("../../models/Post");
+//import Validation
+const validatePostInput = require("../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Tests post route
@@ -10,5 +17,28 @@ router.get("/test", (req, res) => {
     status: 201
   });
 });
+
+// @route   POST api/posts
+// @desc    Create Post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    //Check validation
+    if (!isValid) {
+      //If any errors send error
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
