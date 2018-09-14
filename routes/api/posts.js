f4d78bb6a5d0c1dec26eee1a238ab0c966702f0a
Simranjit Singh <simranjit.boyal@gmail.com>
2018-09-13 22:56:52 -0600

    Added GET Route to fetch all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -18,6 +18,16 @@ router.get("/test", (req, res) => {
   });
 });
 
+// @route   GET api/posts
+// @desc    Get Post
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404));
+});
+
 // @route   POST api/posts
 // @desc    Create Post
 // @access  Private
