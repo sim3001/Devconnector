c5071da02ad21c613e8b362956534274bc1b36f1
Simranjit Singh <simranjit.boyal@gmail.com>
2018-09-15 16:07:55 -0600

    Setup React-Router-Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+
+          <Route exact path="/" component={Landing} />
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
