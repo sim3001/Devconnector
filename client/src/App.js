1bf0af614e0b4e1567daaee04c2a1421136e6b6c
Simranjit Singh <simranjit.boyal@gmail.com>
2018-09-15 16:27:58 -0600

    Added Linking to Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+//Load Components
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
+//Load Styles
 import "./App.css";
 
 class App extends Component {
@@ -15,6 +19,11 @@ class App extends Component {
           <Navbar />
 
           <Route exact path="/" component={Landing} />
+
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
 
           <Footer />
         </div>
