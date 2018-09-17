2680b1a32fdde55bbf43f667487ff08bfecc0abf
Simranjit Singh <simranjit.boyal@gmail.com>
2018-09-17 17:33:02 -0600

    Added componentDidMount() to protect unwanted components displaying

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 
 //Import redux store
@@ -26,6 +26,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   //Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token?
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+    //TODO: Clear current profile
+    //Redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
