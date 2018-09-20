0cd8e59baa4c47ec2e6d599a66bec66c563cc94d
Simranjit Singh <simranjit.boyal@gmail.com>
2018-09-20 12:59:05 -0600

    Added PrivateRoute component to render dashboard depending on AUTH

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
@@ -9,6 +9,8 @@ import { Provider } from "react-redux";
 
 //Import redux store
 import store from "./store";
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 //Load Components
 import Navbar from "./components/layout/Navbar";
@@ -53,7 +55,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
